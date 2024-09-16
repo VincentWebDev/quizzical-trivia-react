159e79a49051d086b60111f7a9f1b76d688707fd
Vincent Croos <vincent.croos@gmail.com>
2024-09-16 08:51:08 -0500

    check fetch

--- src/App.jsx
+++ src/App.jsx
@@ -5,14 +5,25 @@ import "./App.css";
 
 function App() {
   const [button, setButton] = useState("Start Quiz");
+  const [displayQuestions, setDisplayQuestions] = useState(true);
+
+  function startQuiz() {
+    setDisplayQuestions(true);
+    console.log("Quiz started");
+  }
 
   return (
     <main>
-      <h1>Quizzical</h1>
-      <Questions />
-      <button type="button" value="start-quiz">
-        {button}
-      </button>
+      {(displayQuestions && (
+        <Questions displayQuestions={displayQuestions} />
+      )) || (
+        <>
+          <h1>Quizzical</h1>
+          <button type="button" value="start-quiz" onClick={startQuiz}>
+            Start Quiz
+          </button>
+        </>
+      )}
     </main>
   );
 }
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,23 +1,64 @@
 import { useState, useEffect } from "react";
+import { decode } from "html-entities";
 import "../App.css";
 // API https://opentdb.com/api.php?amount=5&type=multiple
 // API https://jsonplaceholder.typicode.com/posts/1 => placeholder
 
-export default function Questions() {
-  const [questions, setQuestions] = useState();
+export default function Questions(props) {
+  const [questions, setQuestions] = useState(["initial array"]);
+  const { displayQuestions } = props;
+  const [questionsDiv, setQuestionsDiv] = useState(
+    <div>here are the questions</div>
+  );
+  // console.log("guestionsDiv is " + questionsDiv);
 
   useEffect(() => {
-    const getQuestions = async () => {
-      const response = await fetch(
-        "https://opentdb.com/api.php?amount=5&type=multiple"
-      );
-      const data = await response.json();
-      setQuestions(data.results);
-    };
-    getQuestions();
-    console.log(questions);
-    console.log("rendering");
-  }, []);
+    console.log("useEffect runs");
+    // console.log("displayQuestions is " + displayQuestions);
 
-  return <div>Questions:</div>;
+    const getQuestions = async () => {
+      try {
+        console.log("fetch runs");
+        const response = await fetch(
+          "https://opentdb.com/api.php?amount=5&type=multiple"
+        );
+
+        if (!response.ok) throw new Error(response.status);
+        const data = await response.json();
+        const results = data.results;
+        setQuestions(
+          results.map((result) => {
+            const questionsArray = {
+              question: result.question,
+              possible_answer: [
+                ...result.incorrect_answers,
+                result.correct_answer,
+              ],
+              correct_answer: result.correct_answer,
+            };
+            return questionsArray;
+          })
+        );
+        setQuestionsDiv(
+          <div>
+            {questions &&
+              questions.map((question, index) => {
+                console.log("question.question is " + question.question);
+                return <div key={index}>{question.question}</div>;
+              })}
+          </div>
+        );
+        console.log("setQuestionsDiv should have ran");
+      } catch (error) {
+        console.error(error);
+      }
+      // console.log("questions is " + questions);
+      // console.log("guestionsDiv is " + questionsDiv);
+    };
+
+    getQuestions();
+    // console.log("questions is " + questions);
+    // console.log("guestionsDiv is " + questionsDiv);
+  }, []);
+  return <div>Questions: {questionsDiv}</div>;
 }
