dcbc0225e29bb9435b35793d628ffd3e10ac5507
Vincent Croos <vincent.croos@gmail.com>
2024-09-29 10:11:45 -0400

    app now working

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,54 @@
-import { useState } from "react";
+import { useState, useRef } from "react";
 import Questions from "./components/Questions";
 import viteLogo from "/vite.svg";
 import "./App.css";
 
 function App() {
-  const [button, setButton] = useState("Start Quiz");
-  const [displayQuestions, setDisplayQuestions] = useState(true);
+  const [buttonMessage, setButtonMessage] = useState("Start Quiz");
+  const quizStatus = useRef("home");
 
-  function startQuiz() {
-    setDisplayQuestions(true);
-    console.log("Quiz started");
+  function handleClick() {
+    switch (quizStatus.current) {
+      case "home":
+        quizStatus.current = "getQuestions";
+        setButtonMessage("Check Answers");
+
+        break;
+      case "playing":
+        quizStatus.current = "checkAnswers";
+        setButtonMessage("Play Again");
+        break;
+      case "viewingAnswers":
+        quizStatus.current = "getQuestions";
+        setButtonMessage("Check Answers");
+        break;
+      default:
+        quizStatus.current = "home";
+        setButtonMessage("Start Quiz");
+    }
+  }
+
+  function updateQuizStatus(status) {
+    quizStatus.current = status;
+  }
+
+  function getQuizStatus() {
+    return quizStatus.current;
   }
 
   return (
     <main>
-      {(displayQuestions && (
-        <Questions displayQuestions={displayQuestions} />
-      )) || (
-        <>
-          <h1>Quizzical</h1>
-          <button type="button" value="start-quiz" onClick={startQuiz}>
-            Start Quiz
-          </button>
-        </>
-      )}
+      {(quizStatus.current !== "home" && (
+        <Questions
+          quizStatus={quizStatus.current}
+          updateQuizStatus={updateQuizStatus}
+          getQuizStatus={getQuizStatus}
+          buttonMessage={buttonMessage}
+        />
+      )) || <>{quizStatus.current === "home" && <h1>Quizzical</h1>}</>}
+      <button type="button" onClick={handleClick}>
+        {buttonMessage}
+      </button>
     </main>
   );
 }
--- src/components/Questions.jsx
+++ src/components/Questions.jsx
@@ -1,64 +1,170 @@
 import { useState, useEffect } from "react";
 import { decode } from "html-entities";
+import { nanoid } from "nanoid";
 import "../App.css";
-// API https://opentdb.com/api.php?amount=5&type=multiple
-// API https://jsonplaceholder.typicode.com/posts/1 => placeholder
 
 export default function Questions(props) {
-  const [questions, setQuestions] = useState(["initial array"]);
-  const { displayQuestions } = props;
-  const [questionsDiv, setQuestionsDiv] = useState(
-    <div>here are the questions</div>
-  );
-  // console.log("guestionsDiv is " + questionsDiv);
+  const [questions, setQuestions] = useState([]);
+  const [scoreMessage, setScoreMessage] = useState();
+  const { updateQuizStatus, getQuizStatus } = props;
 
+  // updates questions depending on status of game
   useEffect(() => {
-    console.log("useEffect runs");
-    // console.log("displayQuestions is " + displayQuestions);
+    if (getQuizStatus() === "getQuestions") {
+      getQuestions();
+      updateQuizStatus("playing");
+    } else if (getQuizStatus() === "checkAnswers") {
+      verifyAnswers();
+      updateQuizStatus("viewingAnswers");
+    } else if (getQuizStatus() === "getQuestions") {
+      getQuestions();
+    }
+  });
 
-    const getQuestions = async () => {
-      try {
-        console.log("fetch runs");
-        const response = await fetch(
-          "https://opentdb.com/api.php?amount=5&type=multiple"
-        );
+  async function getQuestions() {
+    let count = 0;
+    console.log("getQuestions ran " + ++count);
+    try {
+      const response = await fetch(
+        "https://opentdb.com/api.php?amount=5&type=multiple"
+      );
 
-        if (!response.ok) throw new Error(response.status);
-        const data = await response.json();
-        const results = data.results;
-        setQuestions(
-          results.map((result) => {
-            const questionsArray = {
-              question: result.question,
-              possible_answer: [
-                ...result.incorrect_answers,
-                result.correct_answer,
-              ],
-              correct_answer: result.correct_answer,
-            };
-            return questionsArray;
-          })
-        );
-        setQuestionsDiv(
-          <div>
-            {questions &&
-              questions.map((question, index) => {
-                console.log("question.question is " + question.question);
-                return <div key={index}>{question.question}</div>;
-              })}
+      if (!response.ok) throw new Error(response.status);
+      const data = await response.json();
+      const results = data.results;
+      setQuestions(
+        results.map((result) => {
+          const questionObj = {
+            question: decode(result.question),
+
+            possible_answers: [
+              ...result.incorrect_answers.map((result) => {
+                return {
+                  answer: decode(result),
+                  selected: false,
+                };
+              }),
+              {
+                answer: decode(result.correct_answer),
+                selected: false,
+              },
+            ].sort(() => Math.random() - 0.5),
+            correct_answer: decode(result.correct_answer),
+            id: nanoid(),
+          };
+          return questionObj;
+        })
+      );
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  function getQuestionsDiv() {
+    return questions.map((question) => {
+      return (
+        <div className="questions-container" key={question.id + "questions"}>
+          <div className="question" aria-label="question" key={question.id}>
+            {question.question}
           </div>
-        );
-        console.log("setQuestionsDiv should have ran");
-      } catch (error) {
-        console.error(error);
-      }
-      // console.log("questions is " + questions);
-      // console.log("guestionsDiv is " + questionsDiv);
-    };
+          <div
+            className="possible_answers"
+            key={question.id + "possible answers"}
+          >
+            <ul aria-label="possible answers" aria-multiselectable="false">
+              {getAnswers(question.possible_answers, question)}
+            </ul>
+          </div>
+        </div>
+      );
+    });
+  }
 
-    getQuestions();
-    // console.log("questions is " + questions);
-    // console.log("guestionsDiv is " + questionsDiv);
-  }, []);
-  return <div>Questions: {questionsDiv}</div>;
+  function getAnswers(answers, question) {
+    const answersList = [];
+
+    for (const answer of answers) {
+      answersList.push(
+        <li
+          className={getAnswersClassName(answer, question)}
+          key={question.id + answer.answer}
+          aria-label="answer"
+          aria-selected={answer.selected}
+          onClick={(event) => {
+            answerSelected(event, question.question);
+          }}
+        >
+          {answer.answer}
+        </li>
+      );
+    }
+
+    return answersList;
+  }
+
+  function getAnswersClassName(answer, question) {
+    if (getQuizStatus() === "playing")
+      return answer.selected ? "selected-answer" : "default-answer";
+    else if (getQuizStatus() === "viewingAnswers") {
+      if (answer.answer === question.correct_answer) return "correct-answer";
+      else if (answer.selected === true) return "wrong-answer";
+      else {
+        return "faded";
+      }
+    }
+  }
+  function answerSelected(event, thisQuestion) {
+    // prevent answer change after viewing answers
+    if (getQuizStatus() === "viewingAnswers") return;
+
+    setQuestions((prevQuestions) => {
+      return prevQuestions.map((question) => {
+        return {
+          ...question,
+          possible_answers: question.possible_answers.map((prevAnswer) => {
+            if (thisQuestion === question.question) {
+              return {
+                answer: prevAnswer.answer,
+                selected:
+                  event.target.textContent === prevAnswer.answer ? true : false,
+              };
+            } else {
+              return {
+                answer: prevAnswer.answer,
+                selected: prevAnswer.selected,
+              };
+            }
+          }),
+        };
+      });
+    });
+  }
+
+  function verifyAnswers() {
+    let currentScore = 0;
+    const selectedAnswers = questions.map((question) => {
+      for (let i = 0; i < question.possible_answers.length; i++)
+        if (question.possible_answers[i].selected === true)
+          return question.possible_answers[i].answer;
+    });
+
+    const correctAnswers = questions.map((question) => question.correct_answer);
+
+    for (let i = 0; i < selectedAnswers.length; i++) {
+      if (selectedAnswers[i] === correctAnswers[i]) {
+        currentScore++;
+      }
+    }
+
+    setScoreMessage(`You scored ${currentScore}/5 correct answers`);
+  }
+
+  return (
+    <div className="questions-container">
+      {getQuestionsDiv()}
+      <div className="score" aria-label="score">
+        {props.quizStatus === "checkAnswers" && scoreMessage}
+      </div>
+    </div>
+  );
 }
